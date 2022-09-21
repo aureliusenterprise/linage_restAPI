7751cdaf9033e6f6e33200dc1b2cb46f3b957e3d
Gabriella Pascha <gabriella.pascha@gmail.com>
2022-09-21 12:35:02 +0200

    reference

--- m4i_lineage_rest_api/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lin_api import *
-from .app import *
-from .settings import *
-from .conftest import *
-from .functions import *
--- m4i_lineage_rest_api/lin_api/entity/indexPattern_entity/m4i_indexPattern_entity_serializers.py
+++ m4i_lineage_rest_api/lin_api/entity/indexPattern_entity/m4i_indexPattern_entity_serializers.py
@@ -1,4 +1,4 @@
-from .lin_api.m4i_generic_entity_serializers import *
+from ...m4i_generic_entity_serializers import *
 
 m4i_indexPattern_entity_model = api.inherit('model_m4i_indexPattern_entity', m4i_generic_entity_model, {
     'indexPattern': fields.String(required=True, description='The indexPattern used by the save object index-pattern'),
